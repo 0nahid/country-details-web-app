e920cd4df04a860c026047f790d5a2f4c402616f
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-09-15 02:48:06 +0600

    structure ready

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
-import CountryList from './CountryList/CountryList';
+import CountryList from '../CountryList/CountryList';
 
 const Country = () => {
     const [country, setCountry] = useState([]);
